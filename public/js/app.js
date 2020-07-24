6000f37cc4e3e56578c7735cfe491577c3e86f6b
Narendra Verma <nverma1@bechtel.com>
2020-07-24 12:36:11 +0530

    Commented the console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log("Client Side JScript loaded")
+//console.log("Client Side JScript loaded")
 
 // fetch("http://puzzle.mead.io/puzzle").then((response) => {
 //     response.json().then((data) => {
