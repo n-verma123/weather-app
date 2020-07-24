eda19057cf5fb5f3288074f80166f574da7bec12
Narendra Verma <nverma1@bechtel.com>
2020-07-24 14:25:14 +0530

    Setup App For Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ addressForm.addEventListener('submit',(e)=>{
     msg2.textContent=""
     const location = search.value
     console.log('Input Value : '+location)
-    fetch("http://localhost:3000/weather?address='"+location+"'").then((response) =>{
+    fetch("/weather?address='"+location+"'").then((response) =>{
         response.json().then((data) =>{
             if (data.error) msg1.textContent=data.error
             else{
